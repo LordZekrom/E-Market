4c8368cbd1f67e9ccdbac962424f7deecc519eca
brocolisverde <brunoribeiroribeiro98@outlook.com>
2024-07-22 08:52:19 -0300

    Burno

---
+++ perfil/perfil_sandbox.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sandbox</title>
+</head>
+<body>
+    <a href="perfil\perfil.php"><img src=""></a>
+</body>
+</html>
