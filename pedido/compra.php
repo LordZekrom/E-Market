fa9e72a6ae8c52daf73903a7214f6ccfb01d4781
RafaelM007 <rafaelmoreiraxavier09@gmail.com>
2024-09-27 10:31:02 -0300

    aaaaaa

--- pedido/compra.php
+++ pedido/compra.php
@@ -11,24 +11,26 @@
         }
 
         body {
-            font-family: Arial, sans-serif;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
             margin: 0;
             padding: 0;
+
         }
 
         .product {
             border: 1px solid #ccc;
-            border-radius: 10px;
-            padding: 20px;
-            width: 300px;
-            height: 450px;
-            margin: 15px;
+            padding: 10px;
+            width: 220px;
+            height: 380px;
+            margin: 4px;
+            margin-top: 20px;
             display: flex;
             flex-direction: column;
             justify-content: space-between;
             text-align: center;
             background-color: #f9f9f9;
             transition: box-shadow 0.3s;
+            align-items:center;
         }
 
         .product:hover {
@@ -36,16 +38,21 @@
         }
 
         .product img {
-            max-width: 100%;
-            height: 150px;
+            max-width: 65%;
             object-fit: cover;
             border-radius: 8px;
-            margin-bottom: 15px;
+            margin-bottom: 2px;
+            height: auto;
+        
         }
 
+        .product h3 {
+            color: #333;
+            margin: 10px 0;
+        }
         .product h4 {
             font-size: 1.2em;
-            color: #333;
+            color: #00991f;
             margin: 10px 0;
         }
 
@@ -53,6 +60,12 @@
             font-size: 1em;
             color: #666;
             margin: 5px 0;
+        }
+
+        .product h6 {
+            color: #333;
+            margin: 10px 0;
+            font-size: 0.9em;
         }
 
         .product table {
@@ -86,9 +99,8 @@
             margin-bottom: 10px;
             transition: background-color 0.3s;
         }
-
         .product button:hover {
-            background-color: #1abc9c;
+            background-color: #21a3b4;;
         }
 
         /* Flex container for products */
@@ -148,19 +160,17 @@
            
             echo "<div class='product'>
                 <img src='../produtos/imagens/" . $row['fotoProduto'] . "' />
-                <h4>" . $row['nomeProduto'] . "</h4>
+                " . $row['nomeProduto'] . "
                 <table>
                     <tr>
-                        <th>Preço</th>
-                        <td>R$" . $row['precoProduto'] . "</td>
+                        <h4>R$" . $row['precoProduto'] . "</h4>
                     </tr>
+                    <br>
                     <tr>
-                        <th>Descrição</th>
-                        <td>" . $row['descricaoProduto'] . "</td>
+                        " . $row['descricaoProduto'] . "
                     </tr>
                 </table>
                 <button onclick=\"window.location.href='addCarrinho.php?codigoProduto=$codigoProduto'\">Comprar</button>
-                <button onclick=\"window.location.href='addCarrinho.php?codigoProduto=$codigoProduto'\">Adicionar ao carrinho</button>
             </div>";
         }
     ?>
