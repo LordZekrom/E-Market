cd13c370432b7013510f6b4323df6c200dae496b
LordZekrom <h0wlpendrag0n.gm@gmail.com>
2024-09-27 11:13:56 -0300

    a

--- perfil/cadastra.php
+++ perfil/cadastra.php
@@ -1,21 +1,34 @@
 <html>
 	<head><title>Login</title>
-	<link rel="stylesheet" type="text/css" href="style.css" />
+    	<link rel="stylesheet" href="../perfil.css">
+		<link rel="stylesheet" type="text/css" href="cadastra.css" />
 	</head>
 	<body>
-		<h3>Login</h3>
 		<form method="post" action="salva.php">
-			<label>CPF:</label>
-			<input name="cpf" /><br>
-			<label>Nome:</label>
-			<input name="nome" /><br>
-			<label>Email:</label>
-			<input name="email" /><br>
-			<label>Senha:</label>
-			<input type="password" name="senha" /><br>
-			<label>Foto de Perfil:</label>
-            <input type="file" name="fotoPerfil" /><br>
-			<button type="submit">Cadastrar</button>
+  			<div class="login-box">
+				<h3 class="form-title">Cadastro</h3>
+    				<div class="input-container">
+      					<label>CPF:</label>
+     					<input name="cpf" placeholder="Digite seu CPF" />
+    				</div>
+    				<div class="input-container">
+      					<label>Nome:</label>
+      					<input name="nome" placeholder="Digite seu nome" />
+    				</div>
+    				<div class="input-container">
+      					<label>Email:</label>
+      					<input name="email" placeholder="Digite seu email" />
+    				</div>
+    				<div class="input-container">
+      					<label>Senha:</label>
+      					<input type="password" name="senha" placeholder="Digite sua senha" />
+    				</div>
+    				<div class="input-container">
+      					<label>Foto de Perfil:</label>
+      					<input type="file" name="fotoPerfil" />
+    				</div>
+    			<button type="submit" class="submit">Cadastrar</button>
+  			</div>
 		</form>
 	</body>
 </html>
