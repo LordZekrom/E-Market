f57249ff5ff055d370c0631363c3bba310c2c2be
brocolisverde <brunoribeiroribeiro98@outlook.com>
2024-10-04 08:34:07 -0300

    funfou

--- pedido/delete.php
+++ pedido/delete.php
@@ -8,6 +8,7 @@ $cpf = $_SESSION['cpf'];
 $codigoProduto = $_GET['codigoProduto'];
 $idPedido = $_GET['idPedido'];
 $idItens = $_GET['idItensPedido'];
+$loop = $_GET['quantidadeItensPedido'];
 
 //################### Verificar se exite um PEDIDO com status CARRINHO, se existe, nós pegamos o ID desse pedido.
 
@@ -19,14 +20,16 @@ $stm = $con->prepare($sql);
 $stm->execute();
 $row = $stm->fetch();
 $quantidade = $row['quantidadeProduto'];
-include_once("almentarEstoque.php");
+for(;$loop>0;$loop--){
+    include("almentarEstoque.php");
+}
 
 ###################### Remoção dos itens do pedido
 $sql = "DELETE FROM itenspedido WHERE idProduto = $codigoProduto AND idPedido = $idPedido AND idItensPedido = $idItens";
 $stm = $con->prepare($sql);
 $r = $stm->execute();
 
-$url = ""
+$url = "";
 if($r){
     print "<script>alert('Carrinho finalizado!')</script>";
     $url = "Location:../pedido/carrinho.php";
